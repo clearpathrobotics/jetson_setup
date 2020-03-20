21bf0b1e7c7b3cf610c2af9ff3da41248213b991
Chris I-B <civerachb@clearpathrobotics.com>
2020-03-20 11:34:55 -0400

    Add an additional step to install OS updates & useful additional packages before proceeding with the rest of the installation

--- scripts/jetson_setup.sh
+++ scripts/jetson_setup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+############## INSTALL UPDATES + USEFUL ADDITIONAL PACKAGES ##############
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get install -y nano bash-completion git
+
 ############## ADD ROS KINETIC SOURCE ##############
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
